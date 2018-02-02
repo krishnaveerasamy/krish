a4aa460e92b90386bdfdc2235fd6d7d674ea0145
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-02 16:32:12 +0530

    krishna2.c
    program

---
+++ krishna2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int num,digit;
+printf("\n enter the number");
+scanf("%d",&num);
+do
+{
+digit=num%10;
+num=num/10;
+printf("%d",digit);
+}while(num!=0);
+getch();
+}
