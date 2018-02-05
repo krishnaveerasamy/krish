16889839057161a58c77f4db6f14d24c585b4236
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-05 16:21:16 +0530

    krishnaveni2.c
    program

---
+++ krishnaveni2.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+#include<sting.h>
+void main()
+{
+int number;
+printf("enter the number to check isomorphic number");
+scanf("%d",&number);
+if(number>>1)
+printf(%d",& isomorphic number);
+else
+printf("%d,&not a iosomorphic number");
+getch();
+}
