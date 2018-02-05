c80d37f4542c9141c595689279cd5aecaa44ded9
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-05 16:43:58 +0530

    krishnaveni6.c
    program

---
+++ krishnaveni6.c
@@ -0,0 +1,12 @@
+#include<stdio.>
+#include<conio.h>
+void main()
+{
+int a,b,sum;
+clrcsr();
+printf("enter the values of a and b:");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("sum is",sum);
+getch();
+}
