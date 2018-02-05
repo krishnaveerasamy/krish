898ea1ed45da0b10aaac32c1f7b7461647123bb5
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-05 16:25:27 +0530

    krishnaveni3.c
    program

---
+++ krishnaveni3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter a number to cneck even or odd");
+scanf("%d",&number);
+if(number>>1)
+printf("%d,is a even number);
+else
+printf("%d",is a odd number);
+getch();
+}
