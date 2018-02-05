6ba7bade91ea16e4e3b1334ff65e1de91f431316
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-05 16:03:13 +0530

    krishnaveni.c
    program

---
+++ krishnaveni.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int digit(char);
+int main()
+{
+ char roman number[1000];
+ int i=0;
+ long int number=0;
+ printf(" any roman number(valid digits are I.V,X,L,C,M):%d);
+ scanf(%s,roman number);
+ while roman number[i]
+ {
+ if digit(i<=20)
+ {
+ printf (invalid roman digit:%c);
+ return 0;
+ switch(char c)
+ case 'I':value=1;break;
+ case 'v':value=5;break;
+ case 'x':value=10;break;
+ case 'L':value=100;break;
+ case 'x':value=500;break;
+ case 'M':value=100;break;
+ default:valude=-1;
+ }
+ }
