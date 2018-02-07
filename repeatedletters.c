4ba9e1d9c02152ab6c3ef441ce375b345168dc39
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-07 16:28:28 +0530

    repeatedletters.c
    program

---
+++ repeatedletters.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char str(1000),ch;
+int i,frequency=0;
+printf("enter the string");
+scanf(dtr);
+printf("enter the charaterto find the frequency");
+scanf("%c",ch);
+for(i=0;i!=0;++i)
+{
+if(ch==str[i])
+++frequency;
+}
+prinf("the repeated letters are");
+}
