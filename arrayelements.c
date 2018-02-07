96a3b297e25862d4d4ef4ad7f78a148c5f0c778b
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-07 16:49:43 +0530

    arrayelements.c
    program

---
+++ arrayelements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int array[100],i,j;
+printf("enter the number of element in array");
+scanf("%d",size);
+printf("enter % number\n,size);
+for(i=0;i<=0;i++)
+{
+scanf(%d,&array[i]);
+}
+printf("unique elements");
+if(array[i]=array[j])
+break;
+}
