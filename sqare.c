f91217720a599471c13059ca3a9757f774e46c03
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-07 16:10:11 +0530

    square.c
    program

---
+++ sqare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+int fact=1,n;
+printf("enter the  number");
+scanf("%d",&n);
+for(int i=0;i<=n;i++)
+{
+fact=fact*i:
+printf("the factorial of the given num is %d",&fact);
+getch();
+}
