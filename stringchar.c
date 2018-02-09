56cda1d106e48ad4e812a333290cef9911377990
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-09 16:12:56 +0530

    stringchar.c
    program

---
+++ stringchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+char str[20],str2[20];
+int res;
+puts("enter string1:);
+gets(str1);
+puts("enter string2:");
+get(str2);
+res=strcmpi(str1,str2);
+if(res==0)
+puts("strings are equal");
+else
+puts("string are not equal");
+}
