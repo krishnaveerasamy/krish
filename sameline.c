1295c9f9c61f68569c177ff6cc600d0e3db79c77
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-09 16:20:17 +0530

    sameline.c
    program

---
+++ sameline.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char source[]="fresh 2 refresh";
+char target[]="c tutoral";
+printf("\nsourcestring=%s",source);
+printf("\n target string=%s",target);
+strcat(target string agter strcat()=%s",target);
+}
