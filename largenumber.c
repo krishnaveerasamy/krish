f976a1c71e56bbb0e5dc5f13df6787c4ef8f3fd7
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-09 15:42:14 +0530

    largenumber.c
    program

---
+++ largenumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+int n%num=0,i=0;
+printf(enter the numbers\t");
+scanf(%d,&n);
+for(i=2;i<n;i++)
+if(n/i==0)
+num=num=1;
+break;
+}
+if(num==1)
+printf("the large number is");
+getch();
+}
