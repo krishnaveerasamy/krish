7446875841c65a7fef966c11edb95f13902e74aa
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-09 15:55:36 +0530

    number of string.c
    program

---
+++ number of string.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+char*ptr="dear";
+char name[50]="reader";
+printf("the length of string:\n");
+printf("hello is %d\n",strlen("hhello");
+printf("dear is %d\n",strlen(ptr));
+printf("reader is %d\n",strlen(name));
+}
