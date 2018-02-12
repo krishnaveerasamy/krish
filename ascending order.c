d85a7e3494962e78c81618e09eaeb26d7fd062ed
krishnaveerasamy <36039163+krishnaveerasamy@users.noreply.github.com>
2018-02-12 16:30:59 +0530

    ascending order.c
    program

---
+++ ascending order.c
@@ -0,0 +1,14 @@
+#iclude<stdio.h>
+#include<conio.h>
+void main()
+{
+inti,n;
+int a[i];
+printf("\n enter the n number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter the array n element");
+scanf("%d",&n);
+}
+}
